b3cfcc93a3b594cb25e1f42732d8a20fdaecde0f
Kevin Lai <kevinlaisoftware@gmail.com>
2023-10-18 16:03:26 -0400

    Add latest version

---
+++ Node.m
@@ -0,0 +1,11 @@
+classdef Node
+   properties
+      prevPathRow = [];
+      prevPathCol = [];
+   end
+   methods
+      function r = addPrevPath(x,y)
+         r = round([obj.Value],2);
+      end
+   end
+end
